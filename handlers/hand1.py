979754db64b7b1007dd4223b964221802250e848
mrtwister <guskuskudu@gmail.com>
2024-04-09 14:31:18 +0700

    Update hand1.py

--- handlers/hand1.py
+++ handlers/hand1.py
@@ -3,3 +3,10 @@ def test(hashMap,_files=None,_data=None):
       hashMap.put("toast", "123")
 
   return hashMap
+
+def init_on_start(hashMap,_files=None,_data=None):
+
+   hashMap.put("SQLConnectDatabase","test1.DB")
+   hashMap.put("SQLExec",json.dumps({"query":"create table IF NOT EXISTS Record(id integer primary key autoincrement,art text, barcode text, name text, qty real)","params":""}))
+
+   return hashMap
