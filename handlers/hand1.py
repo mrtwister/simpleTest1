ab79a3d5ae8ecb4ab93a59dc25e4af2d21750c79
mrtwister <guskuskudu@gmail.com>
2024-04-09 15:07:50 +0700

    Update hand1.py

--- handlers/hand1.py
+++ handlers/hand1.py
@@ -10,3 +10,15 @@ def init_on_start(hashMap,_files=None,_data=None):
    hashMap.put("SQLExec",json.dumps({"query":"create table IF NOT EXISTS Record(id integer primary key autoincrement,art text, barcode text, name text, qty real)","params":""}))
 
    return hashMap
+
+def input_qty(hashMap,_files=None,_data=None):
+
+   sql = sqlClass()
+   success=sql.SQLExec("insert into Record(barcode,name,qty) values(?,?,?)",hashMap.get('barcode')+","+hashMap.get("nom")+","+str(hashMap.get("qty")))
+
+   if success:
+           hashMap.put("ShowScreen","Сканирование")
+           hashMap.put("toast","Добавлено")
+
+
+   return hashMap
