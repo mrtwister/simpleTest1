70a412575815a6847f9340cc2bd4d4532be76b71
mrtwister <guskuskudu@gmail.com>
2024-04-09 15:43:42 +0700

    Пока СУБД не работает тестирую переключение

--- handlers/hand1.py
+++ handlers/hand1.py
@@ -13,12 +13,13 @@ def init_on_start(hashMap,_files=None,_data=None):
 
 def input_qty(hashMap,_files=None,_data=None):
 
-   sql = sqlClass()
-   success=sql.SQLExec("insert into Record(barcode,name,qty) values(?,?,?)",hashMap.get('bcode')+","+hashMap.get("text_product")+","+str(hashMap.get("qty")))
+   #sql = sqlClass()
+   #success=sql.SQLExec("insert into Record(barcode,name,qty) values(?,?,?)",hashMap.get('bcode')+","+hashMap.get("text_product")+","+str(hashMap.get("qty")))
+   success = True
 
    if success:
            hashMap.put("ShowScreen","Сканирование")
-           hashMap.put("toast","Добавлено")
+           hashMap.put("toast","Добавлено" + hashMap.get("text_product"))
 
 
    return hashMap
