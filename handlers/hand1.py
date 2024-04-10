5ae35564230b14e318e87d21bed67298faa8f2fb
mrtwister <guskuskudu@gmail.com>
2024-04-10 10:22:24 +0700

    Update hand1.py

--- handlers/hand1.py
+++ handlers/hand1.py
@@ -25,43 +25,44 @@ def input_qty(hashMap,_files=None,_data=None):
 	return hashMap
 
 def on_start_barcode(hashMap,_files=None,_data=None):
+	from ru.travelfood.simple_ui import SimpleSQLProvider as sqlClass
 
-		rows=[]
+	rows=[]
 
-		table  = {
-		"type": "table",
-		"textsize": "20",
+	table  = {
+	"type": "table",
+	"textsize": "20",
 
-		"columns": [
+	"columns": [
+	{
+			"name": "barcode",
+			"header": "[Barcode]",
+			"weight": "2"
+	},
+	{
+			"name": "name",
+			"header": "Name",
+			"weight": "2"
+	},
 		{
-				"name": "barcode",
-				"header": "[Barcode]",
-				"weight": "2"
-		},
-		{
-				"name": "name",
-				"header": "Name",
-				"weight": "2"
-		},
-			{
-				"name": "qty",
-				"header": "[Qty]",
-				"weight": "1"
-		}
-		]
-		}
+			"name": "qty",
+			"header": "[Qty]",
+			"weight": "1"
+	}
+	]
+	}
 
-		sql = sqlClass()
-		res = sql.SQLQuery("select * from Record","")
+	sql = sqlClass()
+	res = sql.SQLQuery("select * from Record","")
 
-		records = json.loads(res)
-		for record in records:
-		    rows.append({"barcode":record['barcode'],"name":record['name'],"qty":str(record['qty'])})
+	records = json.loads(res)
+	for record in records:
+			rows.append({"barcode":record['barcode'],"name":record['name'],"qty":str(record['qty'])})
 
-		table['rows'] =rows
-		hashMap.put("table",json.dumps(table))
+	table['rows'] =rows
+	hashMap.put("table",json.dumps(table))
 
-		return hashMap
+	return hashMap
 
 def on_start_barcode1(hashMap,_files=None,_data=None):
 
